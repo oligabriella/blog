0e5a46b97fb8bbcfe4817c489947e6b45f9d6d77
Gabriella Oliveira <olivesgab@gmail.com>
2023-01-04 16:35:33 -0300

    delete, edit, update buttons, DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
   # GET /articles or /articles.json
   def index
     @articles = Article.all
@@ -7,8 +9,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1 or /articles/1.json
   def show
-   @article = Article.find(params[:id])
-  end
+     end
 
   # GET /articles/new
   def new
@@ -17,13 +18,40 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1/edit
   def edit
+      end
+
+def update
+    if @article.update(article_params)
+flash[:notice] = "Sua publicação foi editada com sucesso."
+redirect_to @article
+  else
+    render 'edit'
   end
+end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
+    @article = Article.new(article_params)
+    if @article.save
+    flash[:notice] = "Sua publicação foi criada com sucesso."
     redirect_to @article
+  else
+    render 'new'
   end
-
+  end
   
+  def destroy
+        @article.destroy
+    redirect_to articles_path
+  end
+  
+private
+
+def set_article
+@article = Article.find(params[:id])
+end
+  
+def article_params
+  params.require(:article).permit(:title, :description)
+end
+
 end
