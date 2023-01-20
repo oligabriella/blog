3fbd0c66c52cc07689afc56a8a900ebb79f4656c
Gabriella Oliveira <olivesgab@gmail.com>
2023-01-20 13:18:23 -0300

    creating users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_06_222631) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_19_162409) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -54,6 +54,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_06_222631) do
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
