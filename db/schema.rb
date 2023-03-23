1a13fb01a51610de950f19753cd1749446270921
Gabriella Oliveira <olivesgab@gmail.com>
2023-03-23 12:54:54 -0300

    Many to many association between articles and categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_162905) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_154138) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -47,6 +47,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_162905) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
   end
 
   create_table "articles", force: :cascade do |t|
