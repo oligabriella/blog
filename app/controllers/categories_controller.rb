53bc5f0f0726702ac7b6e5b3d7716dcf4b601298
Gabriella Oliveira <olivesgab@gmail.com>
2023-03-31 16:51:38 -0300

    Categories index and view articles

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,6 +21,7 @@ before_action :require_admin, except: [:index, :show]
 
   def show 
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
    private
